359267024274cde340ec248ab14274f69cfd8127
kendo1234 <kendomustdie@googlemail.com>
2015-12-06 14:07:11 +0000

    [Tests] added gmailSendAndRecieveEmailTest

--- src/test/java/GmailSignInTest.java
+++ src/test/java/GmailSignInTest.java
@@ -47,6 +47,77 @@ public class GmailSignInTest {
         Assert.assertTrue("signIn button should exist",driver.findElements(By.id("signIn")).size()>0);
 
     }
+
+    @Test
+    public void gmailSendAndRecieveEmail() {
+
+        //Click Sign In
+
+        driver.get("http://mail.google.com");
+        WebElement usernameTextbox = driver.findElement(By.id("Email"));
+        usernameTextbox.clear();
+        usernameTextbox.sendKeys("kendomustdie@googlemail.com");
+        WebElement nextButton = driver.findElement(By.id("next"));
+        nextButton.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement passwordTextbox = driver.findElement(By.id("Passwd"));
+        passwordTextbox.clear();
+        passwordTextbox.sendKeys("caprimanga");
+        WebElement signInButton = driver.findElement(By.id("signIn"));
+        signInButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Inbox")));
+        Assert.assertTrue("Inbox should exist",driver.findElements(By.partialLinkText("Inbox")).size()>0);
+
+        //Click Compose
+        WebElement composeButton = driver.findElement(By.cssSelector("div[role='button'][gh='cm']"));
+        composeButton.click();
+        //Fill In Recipient
+        WebElement toTextArea = driver.findElement(By.cssSelector("textarea[name='to']"));
+        toTextArea.clear();
+        toTextArea.sendKeys("kendomustdie@googlemail.com");
+
+        //Fill In Subject
+        WebElement subjectTextArea = driver.findElement(By.cssSelector("input[name='subjectbox']"));
+        final String subject = "Gmail Send Email Test";
+        subjectTextArea.clear();
+        subjectTextArea.sendKeys(subject);
+
+        //Fill in Email Body
+        WebElement bodyTextArea = driver.findElement(By.cssSelector("div[aria-label='Message Body']"));
+        final String body = "Yeah Buuuuudeeeeeh";
+        bodyTextArea.clear();
+        bodyTextArea.sendKeys(body);
+
+        //Click Send
+        WebElement sendButton = driver.findElement(By.cssSelector("div[aria-label*=\"Send\" ]"));
+        sendButton.click();
+
+        //Click Inbox Again
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Inbox (1)")));
+        WebElement inboxLinkage = driver.findElement(By.linkText("Inbox (1)"));
+        inboxLinkage.click();
+
+        //Click New Email
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='y6' ] span[id] b")));
+        WebElement newEmail = driver.findElement(By.cssSelector("div[class='y6' ] span[id] b"));
+        newEmail.click();
+
+        //Verify Email Subject And Body Is Correct
+        WebElement subjectArea = driver.findElement(By.cssSelector("h2[class='hP']"));
+        Assert.assertEquals("Email Subject Text should be same", subject, subjectArea.getText());
+        WebElement bodyArea = driver.findElement(By.cssSelector("div[class='nH aHU'] div[dir='ltr']"));
+        Assert.assertEquals("Body Text should be same", body, bodyArea.getText());
+
+        //Sign Out.
+        WebElement profileButton = driver.findElement(By.cssSelector("span[class='gb_Ka gbii']"));
+        profileButton.click();
+        WebElement signOutLinkage = driver.findElement(By.id("gb_71"));
+        signOutLinkage.click();
+
+    }
+
+
     @After
     public void tearDown(){
         driver.close();
